296c090946171468ce9bb3346b96552cfc13e3fa
Prabathvsv <141515238+Prabathvsv@users.noreply.github.com>
2024-06-25 18:40:22 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+# Task 1
+# Explanation: Assigns the value 227 to the variable pi and prints its type and checks if it is an instance of the integer class.
+pi = 227
+print(type(pi))
+print(isinstance(pi, int))
+
+# Task 2
+# Explanation: Demonstrates the use of a reserved keyword 'for' in Python which results in a syntax error.
+# This code is commented out to prevent syntax errors.
+# for = 4
+
+# Task 3
+# Explanation: Calculates simple interest using the provided principal amount, interest rate, and time period.
+principal_amount = 4587
+interest = 5
+time = 3
+
+P = principal_amount
+R = interest
+T = time
+
+simple_interest = (P * R * T) / 100
+print(simple_interest)
